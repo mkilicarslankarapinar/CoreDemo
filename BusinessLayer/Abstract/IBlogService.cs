2ba886237602dc18799b17a3682405b15a0311db
mkilicarslankarapinar <mkilicarslankarapinar@gmail.com>
2023-06-04 00:28:01 +0300

    feat: Blog contents made it.

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void CategoryAdd(Blog blog);
+        void CategoryDelete(Blog blog);
+        void CategoryUpdate(Blog blog);
+        List<Blog> GetList();
+        Blog GetById(int id);
+        List<Blog> GetBlogListWithCategory();
+    }
+}
